b84de21205b7d84e633110eb91175b370eeade9d
Zee Hashmi <hashmiz@nti.internal>
2017-03-15 16:14:22 +1000

    Add project files.

---
+++ UnitTestProject1/TestBase.cs
@@ -0,0 +1,90 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace TigerSpike
+{
+    public class TestBase
+    {
+
+        public IWebDriver driver;
+
+
+        public List<string> listA = new List<String>();
+        public List<string> listB = new List<String>();
+        public List<string> listC = new List<String>();
+        public List<string> listD = new List<String>();
+
+        public void Setbrowser(String browserName)
+        {
+
+            if (browserName.Equals("IE"))
+                driver = new InternetExplorerDriver();
+
+            if (browserName.Equals("Firefox"))
+
+                driver = new FirefoxDriver();
+
+            if (browserName.Equals("Chrome"))
+
+                driver = new ChromeDriver();
+
+            if (browserName.Equals("Safari"))
+
+                driver = new SafariDriver();
+
+            // Add same statement for other browsers
+        }
+
+        /*public static BrowserToNameWith(String browserName)
+        {
+        String[] browsers = { "Chrome", "Firefox", "IE", "Safari"};
+        foreach (String b in browsers)
+        {
+
+        yeild return b;
+        }
+        }
+        */
+
+        public void ReadData()
+        {
+            StreamReader reader = new StreamReader(File.OpenRead(@"C:\Temp\TestData.txt"));
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length >= 4)
+                    {
+                        listA.Add(values[0]);
+                        listB.Add(values[1]);
+                        listC.Add(values[2]);
+                        listD.Add(values[3]);
+                    }
+                    Console.WriteLine(listA);
+                }
+            }
+        }
+
+        public string BrandName()
+        {
+            return this.listA[0];
+
+            
+        }
+
+        public void Closebrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
+
---
+++ UnitTestProject1/TestCase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+
+
+namespace TigerSpike
+{
+    [TestClass]
+    //[TestFixture(typeof(string))]
+    public class TigerSpikeDemoTest : TestBase
+    {
+        public static string BaseUrl = "https://www.amazon.com.au";
+
+
+        String username = "arzoohai@hotmail.com";
+        
+        [Test]
+        public void Login()
+        {
+
+            ReadData();
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(BaseUrl);
+            driver.Manage().Window.Maximize();
+
+            var signin = driver.FindElementById("nav-link-yourAccount");
+            signin.Click();
+
+            var Username = driver.FindElementById("ap_email");
+            Username.SendKeys(username);
+
+            var Password = driver.FindElementById("ap_password");
+            Password.SendKeys("Passw0rd");
+            //Sign in
+            var Loginbutton = driver.FindElementById("signInSubmit");
+            Loginbutton.Click();
+            Thread.Sleep(500);
+
+            //Serch bar
+            var searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
+            Actions action = new Actions(driver);
+            action.ClickAndHold(searchBox);
+            searchBox.SendKeys(BrandName());
+
+            //Search button
+            var searchIcon = driver.FindElement(By.ClassName("nav-input"));
+            searchIcon.Click();
+
+            //Select second product from the search list
+            var selectProduct = driver.FindElementByXPath("//html/body//div/div/div/ul/li[@id][2]//a/h2");
+            selectProduct.Click();
+
+            //Assert text contain on the page
+            string actualvalue = driver.FindElement(By.Id("buybox")).Text;
+            NUnit.Framework.Assert.IsTrue(actualvalue.Contains("Kindle Price"));
+            var buyitnow = driver.FindElementById("one-click-button");
+            buyitnow.Click();
+
+            //Payment Page
+            var paymentpage = driver.FindElementById("set_payments_widget");
+            NUnit.Framework.Assert.IsTrue(actualvalue.Contains("How would you like to pay?"));
+            Thread.Sleep(4000);
+            driver.Quit();
+
+            Thread.Sleep(4000);
+            driver.Quit();
+
+        }
+    }
+}
